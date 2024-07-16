6d781392912f0065725695046e8da42948dee021
elenmohsen <ellen.mohsen85@gmail.com>
2024-07-16 14:00:33 +0300

    createController

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class ExampleController extends Controller
 {
-    
+    function login(){
+        return view('login');
+    }
+
+    function cv(){
+        return view('cv');
+    }
+
+    function contactus(){
+        return view('contactus');
+    }
+
+    function personaldata(Request $request){
+        $name= $request->name;
+        $email= $request->email;
+        $subject= $request->subject;
+        $message= $request->message;
+        return $name . "<br>" . $email . "<br>" . $subject . "<br>" . $message;
+    }
+
 }
+
+
